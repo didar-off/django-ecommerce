30242c9d6d86d7da89bafb5cb7087fe88062efa9
Didar <didarkuliew54@gmail.com>
2025-02-19 17:10:10 +0500

    Models and Data Structure

--- eshop/urls.py
+++ eshop/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('store.urls'))
+    path('', include('store.urls')),
+    path('ckeditor5/', include('django_ckeditor_5.urls')),
 ]
 
 
--- userauths/admin.py
+++ userauths/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from userauths import models
 
-# Register your models here.
+# Admin Models
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email']
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'full_name', 'mobile', 'user_type']
+
+
+admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Profile, ProfileAdmin)
