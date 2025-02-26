8f8dd62e6db55b449c5622c8605d19a690934fda
Didar <didarkuliew54@gmail.com>
2025-02-26 18:42:56 +0500

    Models & Data Structure V1

--- eshop/settings.py
+++ eshop/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env
+
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,6 +145,11 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
+# Setup Custom USer Model
+
+AUTH_USER_MODEL = 'userauths.User'
 
 
 # Default primary key field type
--- userauths/models.py
+++ userauths/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import EmailValidator
 
-# Create your models here.
+
+USER_TYPE = (
+    ('Vendor', 'Vendor'),
+    ('Customer', 'Customer'),
+)
+
+
+def user_directory_path(instance, filename):
+    ext = filename.spli('.')[-1]
+    filename = '%s.%s' % (instance.user.username, filename)
+    return 'user_{0}/{1}'.format(instance.user.username, filename)
+
+
+class User(AbstractUser):
+    username = models.CharField(unique=True, max_length=100)
+    email = models.EmailField(unique=True, validators=[EmailValidator()])
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def clean(self):
+        super().clean()
+        if not self.username:
+            self.username = self.email.split('@')[0]
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.username or self.email
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=user_directory_path, default='default-profile.jpg', blank=True)
+    full_name = models.CharField(max_length=200, null=True, blank=True)
+    mobile = models.CharField(max_length=50, null=True, blank=True)
+    user_type = models.CharField(max_length=50, choices=USER_TYPE, default='Customer')
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = "Profile"
+        verbose_name_plural = "Profiles"
+
+    def __str__(self):
+        return self.user.username or str(self.user)
