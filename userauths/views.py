8fb8227177de01ebe4af8cbdd9bd48dc2730da18
Didar <didarkuliew54@gmail.com>
2025-03-17 14:01:53 +0500

    Register, Login & Logout Functionality using Django

--- userauths/views.py
+++ userauths/views.py
@@ -1,80 +1,95 @@
 from django.shortcuts import render, redirect
-from django.http import JsonResponse
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 
-from userauths import models as userauths_models
 from userauths import forms as userauths_forms
 from vendor import models as vendor_models
+from userauths import models as userauths_models
 
 
 def register_view(request):
     if request.user.is_authenticated:
-        return JsonResponse({'status': 'warning', 'message': 'You are already logged in. Logout if you want to create a new account.'})
-
+        messages.warning(request, 'You are already logged in')
+        return redirect('store:index')
+    
     form = userauths_forms.UserRegisterForm(request.POST or None)
 
     if form.is_valid():
-        try:
-            user = form.save()
+        user = form.save()
 
-            full_name = form.cleaned_data.get('full_name')
-            email = form.cleaned_data.get('email')
-            mobile = form.cleaned_data.get('mobile')
-            password = form.cleaned_data.get('password1')
-            user_type = form.cleaned_data.get('user_type')
+        full_name = form.cleaned_data.get('full_name')
+        email = form.cleaned_data.get('email')
+        mobile = form.cleaned_data.get('mobile')
+        password = form.cleaned_data.get('password1')
+        user_type = form.cleaned_data.get('user_type')
 
-            user = authenticate(email=email, password=password)
-            if user is None:
-                return JsonResponse({'status': 'error', 'message': 'Authentication failed. Please try logging in manually.'})
+        user = authenticate(email=email, password=password)
+        login(request, user)
 
-            login(request, user)
+        messages.success(request, 'Account was created successfully')
 
-            profile = userauths_models.Profile.objects.create(full_name=full_name, mobile=mobile, user=user)
+        profile = userauths_models.Profile.objects.create(
+            full_name = full_name,
+            mobile = mobile,
+            user = user
+        )
 
-            if user_type == 'Vendor':
-                vendor_models.Vendor.objects.create(user=user, store_name=full_name)
-                profile.user_type = 'Vendor'
-            else:
-                profile.user_type = 'Customer'
+        if user_type == 'Vendor':
+            vendor_models.Vendor.objects.create(user=user, store_name=full_name)
+            profile.user_type = 'Vendor'
+        else:
+            profile.user_type = 'Customer'
 
-            profile.save()
+        profile.save()
 
-            return JsonResponse({'status': 'success', 'message': 'Account created successfully and logged in.', 'redirect': 'store:index'})
+        next_url = request.GET.get('next', 'store:index')
+        return redirect(next_url)
+    
+    context = {
+        'form': form
+    }
 
-        except Exception as e:
-            return JsonResponse({'status': 'error', 'message': f'An unexpected error occurred: {e}'})
-    else:
-        errors = []
-        for field, error_list in form.errors.items():
-            for error in error_list:
-                errors.append(f"{field.capitalize()}: {error}")
-
-        return JsonResponse({'status': 'error', 'message': " ".join(errors)})
-
-    return render(request, 'userauths/sign-up.html', {'form': form})
+    return render(request, 'userauths/sign-up.html', context)
 
 
 def login_view(request):
     if request.user.is_authenticated:
-        return JsonResponse({'status': 'warning', 'message': 'You are already logged in'})
-
+        messages.warning(request, 'You are already logged in')
+        return redirect('store:index')
+    
     if request.method == 'POST':
         form = userauths_forms.LoginForm(request.POST or None)
 
         if form.is_valid():
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
+            captcha_verified = form.cleaned_data.get('captcha', False)
 
-            user = authenticate(request, email=email, password=password)
+            if captcha_verified:
+                try:
+                    user_instance = userauths_models.User.objects.get(email=email, is_active=True)
+                    user_authenticate = authenticate(request, email=email, password=password)
 
-            if user is not None:
-                login(request, user)
-                return JsonResponse({'status': 'success', 'message': 'You are logged in', 'redirect': '/'})
+                    if user_instance is not None:
+                        login(request, user_authenticate)
+                        messages.success(request, 'You are logged in')
+
+                        next_url = request.GET.get('next', 'store:index')
+                        return redirect(next_url)
+                    else:
+                        messages.error(request, 'Username or password does not exist')
+                except:
+                    messages.error(request, 'User does not exist')
             else:
-                return JsonResponse({'status': 'error', 'message': 'Invalid email or password'})
+                messages.error(request, 'Captcha Verification failed please try again')
+    else:
+        form = userauths_forms.LoginForm()
 
-    return JsonResponse({'status': 'error', 'message': 'Something went wrong'})
+    context = {
+        'form': form
+    }
+
+    return render(request, 'userauths/sign-in.html', context)
 
 
 def logout_view(request):
@@ -84,6 +99,8 @@ def logout_view(request):
         cart_id = None
 
     logout(request)
-    request.session['cart_id'] = cart_id
 
-    return JsonResponse({'status': 'success', 'message': 'You have been logged out', 'redirect': 'store:index'})
+    request.session['cart_id'] = cart_id
+    messages.success(request, 'You have been logged out')
+
+    return redirect('userauths:sign-in')
